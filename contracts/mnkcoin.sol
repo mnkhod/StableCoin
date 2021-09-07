089c7253b7c0c7ea86360c70fc40bf5e9f7b2b62
mnkhod <mnkhod.dev@gmail.com>
2021-09-07 23:33:10 +0800

    Added ERC20SnapshotUpgradeable , CirculatingSupply , blacklist

--- contracts/mnkcoin.sol
+++ contracts/mnkcoin.sol
@@ -7,14 +7,17 @@ import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20SnapshotUpgradeable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 
 
-contract MnkCoin is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, PausableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+contract MnkCoin is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, PausableUpgradeable, ERC20SnapshotUpgradeable , OwnableUpgradeable, UUPSUpgradeable {
     using SafeMath for uint256;
-    uint256 private transaction_fee;
-    address private transaction_address;
+    uint256 private _transaction_fee;
+    address private _transaction_address;
+    uint256 private _circulatingSupply;
+    mapping(address => bool) private _blacklist;
 
     function initialize() initializer public {
         __ERC20_init("MnkCoin", "MNK");
@@ -22,38 +25,47 @@ contract MnkCoin is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, P
         __Pausable_init();
         __Ownable_init();
         __UUPSUpgradeable_init();
-        transaction_fee=0;
-        transaction_address = msg.sender;
+        _transaction_fee=0;
+        _circulatingSupply=0;
+        _transaction_address = msg.sender;
     }
 
     function transfer(address recipient, uint256 amount) public override returns(bool){
-        require(amount.sub(transaction_fee) > 0, "Not Enough Fee");
+        require(amount.sub(_transaction_fee) > 0, "Not Enough Fee");
+        require (!isBlackListed(recipient), "Token transfer refused. Receiver is on blacklist");
 
-        uint256 realAmount = amount.sub(transaction_fee);
-        _transfer(msg.sender,transaction_address,transaction_fee);
+        uint256 realAmount = amount.sub(_transaction_fee);
+        _transfer(msg.sender,_transaction_address,_transaction_fee);
         _transfer(msg.sender,recipient,realAmount);
         return true;
     }
 
 
     function getTransactionFee() public view returns(uint) {
-        return transaction_fee;
+        return _transaction_fee;
     }
 
     function setTransactionFee(uint fee) public onlyOwner returns(uint){
-        transaction_fee = fee;
-        return transaction_fee;
+        _transaction_fee = fee;
+        return _transaction_fee;
     }
 
     function getTransactionAddress() public view returns(address) {
-        return transaction_address;
+        return _transaction_address;
     }
 
     function setTransactionAddress(address newTransactionAddress) public onlyOwner returns(address) {
-        transaction_address = newTransactionAddress;
-        return transaction_address;
+        _transaction_address = newTransactionAddress;
+        return _transaction_address;
     }
 
+    function isBlackListed(address user) public view returns (bool) {
+        return _blacklist[user];
+    }
+
+    function blacklistUpdate(address user, bool value) public virtual onlyOwner {
+        _blacklist[user] = value;
+    }
 
 
 
@@ -65,17 +77,31 @@ contract MnkCoin is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, P
         _unpause();
     }
 
+
+
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+        _circulatingSupply = _circulatingSupply.add(amount);
     }
 
+    function burn(uint256 amount) public onlyOwner override{
+        _burn(msg.sender, amount);
+        _circulatingSupply = _circulatingSupply.sub(amount);
+    }
+
+    function snapshot() public onlyOwner {
+        _snapshot();
+    }
+
+    // Hooks
     function _beforeTokenTransfer(address from, address to, uint256 amount)
         internal
         whenNotPaused
-        override
+        override(ERC20Upgradeable, ERC20SnapshotUpgradeable)
     {
         super._beforeTokenTransfer(from, to, amount);
     }
+
 
     function _authorizeUpgrade(address newImplementation)
         internal
